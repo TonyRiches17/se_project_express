c296b99ff052055455d58e6922aa102f571932f7
Rich <TonyRiches17@yahoo.com>
2025-09-05 11:41:31 -0400

    Remove Throw Error

--- app.js
+++ app.js
@@ -40,11 +40,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(requestLogger);
 
-app.get("/crash-test", () => {
-  setTimeout(() => {
-    throw new Error("Server will crash now");
-  }, 0);
-});
+// app.get("/crash-test", () => {
+//   setTimeout(() => {
+//     throw new Error("Server will crash now");
+//   }, 0);
+// });
 
 app.post("/signup", validateUserBody, createUser);
 app.post("/signin", validateUserLogin, login);
